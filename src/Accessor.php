efb2e11b0ad07adfbcd0b0997a3434c079b44664
James Coyle <jamesrichardcoyle@gmail.com>
2018-03-08 13:15:36 +0000

    Added accessor trait

---
+++ src/Accessor.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Accessor;
+
+trait Accessor
+{
+	public function __get($name)
+	{
+		$getter = "get{$name}";
+
+		if (method_exists($this, $getter))
+			return $this->$getter();
+	}
+
+	public function __set($name, $value)
+	{
+		$setter = "set{$name}";
+
+		if (method_exists($this, $setter))
+			return $this->$setter($value);
+	}
+
+	public function __isset($name)
+	{
+		$issetter = "isset{$name}";
+		
+		if (method_exists($this, $issetter))
+			return $this->$issetter();
+	}
+
+	public function __unset($name)
+	{
+		$unsetter = "unset{$name}";
+
+		if (method_exists($this, $unsetter))
+			return $this->$unsetter();
+	}
+}
